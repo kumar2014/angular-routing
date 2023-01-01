fcbcb5a1d31156ef73c627cd717aa5d57e289736
kumar2014 <kumaravel.au@gmail.com>
2023-01-01 18:24:50 +0530

    Initial commit

---
+++ src/app/courses/course/course.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CoursesService } from '../../Services/courses.service';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css'],
+})
+export class CourseComponent implements OnInit {
+  constructor(private service: CoursesService, private route: ActivatedRoute) {}
+
+  course;
+  courseId: number;
+
+  ngOnInit(): void {
+    this.courseId = this.route.snapshot.params['id'];
+    //this.courseId = this.route.snapshot.params['name'];
+    this.course = this.service.courses.find((x) => x.id == this.courseId);
+  }
+}
